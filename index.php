9253957baec5b4bb724322dc5d7353b820927818
Adam Gamble <adamgamble.548@gmail.com>
2018-09-15 14:49:08 +0200

    initial commit

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<head>
+
+	<title>Work with Adam</title>
+
+	<link rel="stylesheet" type="text/css" href="assets/css/bootstrap.min">
+
+</head>
+<body>
+
+
+
+
+
+
+
+<script type="text/javascript" src="assets/js/jquery-3.3.1.min"></script>
+</body>
+</html>
